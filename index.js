2c44d850accdf4d7f75e88b37590063557011729
mgrajez <monikagrajez@gmail.com>
2023-12-13 23:00:49 +0100

    Adding Play button functionality

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
 /* --- VARIABLES --- */
-  const choices = ['✊', '🤚', '✌️'];
-  const player1 = document.getElementById('player-1');
+const choices = ["✊", "🤚", "✌️"];
+const player1 = document.getElementById("player-1");
+const player2 = document.getElementById("player-2");
+const resultArea = document.getElementById("result-area");
+const playBtn = document.getElementById("play-btn");
 
-  /* 1a. */
-  const player2 = '?';
-
-  /* 1b. */
-  const resultArea = '?';
-
-  /* 1c. */
-  const playBtn = '?';
-
-  /* This will help check your results */
-  console.log(player2, resultArea, playBtn);
+/* This will help check your results */
+// console.log(player2, resultArea, playBtn);
 
 /* ----------------------------------------------- */
 
 /* --- FUNCTIONS --- */
 
-  const generateChoice = () => {
-    let r = Math.floor(Math.random() * 3);
-    return choices[r]
-  };
+const generateChoice = () => {
+  let r = Math.floor(Math.random() * 3);
+  return choices[r];
+};
 
-  const insertHTML = (choice1, choice2, result) => {
-    player1.innerHTML = choice1;
-    player2.innerHTML = choice2;
-    resultArea.innerHTML = result;
-  };
+const insertHTML = (choice1, choice2, result) => {
+  player1.innerHTML = choice1;
+  player2.innerHTML = choice2;
+  resultArea.innerHTML = result;
+};
 
-  const decideWinner = (a, b) => {
-    if ((a === '✊' && b === '✊') || (a === '🤚' && b === '🤚') || (a === '✌️' && b === '✌️')) {
+const decideWinner = (a, b) => {
+  if (
+    (a === "✊" && b === "✊") ||
+    (a === "🤚" && b === "🤚") ||
+    (a === "✌️" && b === "✌️")
+  ) {
+    /* 3a. */ return "It's a Tie!";
+  } else if (
+    (a === "✊" && b === "✌️") ||
+    (a === "🤚" && b === "✊") ||
+    (a === "✌️" && b === "🤚")
+  ) {
+    /* 3b. */ return "Player 1 wins";
+  } else {
+    /* 3c. */ return "Player 2 wins";
+  }
+};
 
-      /* 3a. */ return "?";
+const play = () => {
+  let choice1 = generateChoice();
+  let choice2 = generateChoice();
+  let result = decideWinner(choice1, choice2);
 
-    } else if ((a === '✊' && b === '✌️') || (a === '🤚' && b === '✊') || (a === '✌️' && b === '🤚')) {
-
-      /* 3b. */ return "?";
-
-    } else {
-
-      /* 3c. */ return "?";
-
-    }
-  };
-
-  const play = () => {
-    let choice1 = generateChoice();
-    let choice2 = generateChoice();
-    let result = decideWinner(choice1, choice2);
-
-    insertHTML(choice1, choice2, result);
-  };
+  insertHTML(choice1, choice2, result);
+};
 
 /* ----------------------------------------------- */
 
 /* --- EVENT LISTENERS --- */
 
-  /* 2. */
-
+/* 2. */
+playBtn.addEventListener("click", play);
 /* ------------------------------- */
